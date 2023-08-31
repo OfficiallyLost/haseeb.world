6179f597e6a50c7e4b3234661d38782af989f35d
Haseeb <me@haseeb.world>
2023-08-31 02:47:00 +0100

    connection tweaks

--- src/Haseeb.js
+++ src/Haseeb.js
@@ -3,16 +3,11 @@ const http = require('http');
 
 class Haseeb {
    constructor(app) {
-      this.app = app;
       this.http = http.createServer(app);
    }
 
    on() {
-      this.app.listen(80, '0.0.0.0', () => console.log('running'));
-
-      // this.http.listen(8080, () => {
-      //    console.log('running');
-      // });
+      this.http.listen(8080, () => console.log('running'));
    }
 
    error(error) {
