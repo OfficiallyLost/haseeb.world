24b245728176ded591b62c3864bbef62beb2dfe5
Haseeb <me@haseeb.world>
2023-08-31 02:47:56 +0100

    add quotes array

--- src/Constants.js
+++ src/Constants.js
@@ -2,8 +2,15 @@
 
 module.exports = {
    colours: {
-      primary: '#010B13',
-      secondary: '#292C33',
-      text: '#BEC3C6',
+      primary: '#171717',
+      secondary: '#444444',
+      text: '#FFF6E0',
    },
+   quotes: [
+      'Your dreams are just a mere representation of your future - only if you believe enough.',
+      'You are either a leader or follower, the only difference is the way of thought. Be open minded to the abstract.',
+      'Space follows all of its laws, in the ideal society - everyone would follow the law. Your mind has laws, follow them.',
+      'If you really want something, imagine you have it - then it will run to you, instead of you chasing it.',
+      'Remove the possibility of failure, now failure will not occur.',
+   ],
 };
