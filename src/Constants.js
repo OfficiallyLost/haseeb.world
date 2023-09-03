d55d68d85d55575d2e38e98cd0668df8b5263e8b
Haseeb <me@haseeb.world>
2023-09-03 22:33:23 +0100

    added quotes

--- src/Constants.js
+++ src/Constants.js
@@ -7,10 +7,13 @@ module.exports = {
       text: '#FFF6E0',
    },
    quotes: [
-      'Your dreams are just a mere representation of your future - only if you believe enough.',
-      'You are either a leader or follower, the only difference is the way of thought. Be open minded to the abstract.',
-      'Space follows all of its laws, in the ideal society - everyone would follow the law. Your mind has laws, follow them.',
-      'If you really want something, imagine you have it - then it will run to you, instead of you chasing it.',
-      'Remove the possibility of failure, now failure will not occur.',
+      'Your dreams are just a mere representation of your future - only if you believe enough',
+      'You are either a leader or follower, the only difference is the way of thought. Be open minded to the abstract',
+      'Each state of mind is a temporary state of mind, happiness or sadness, learn to accept its a never ending cycle - now you\'re unstobbable',
+      'Space follows all of its laws, in the ideal society - everyone would follow the law. Your mind has laws, follow them',
+      'If you really want something, imagine you have it - then it will run to you, instead of you chasing it',
+      'Remove the possibility of failure, now failure will not occur',
+      'Every adversity will always be a disadvantage. Take control your mind, which will take control of your life, now its your unique advantage',
+      '1 of 2 things in the world is energy. 2 forms of energy are positive and negative, choose which mindset you will accquire, there is no inbetween'
    ],
 };
