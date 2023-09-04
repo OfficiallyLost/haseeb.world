a73b3933b6b81a0f63bcedb94738629799fe7758
Haseeb <me@haseeb.world>
2023-09-04 11:28:52 +0100

    grammar mistakes

--- src/Constants.js
+++ src/Constants.js
@@ -9,11 +9,11 @@ module.exports = {
    quotes: [
       'Your dreams are just a mere representation of your future - only if you believe enough',
       'You are either a leader or follower, the only difference is the way of thought. Be open minded to the abstract',
-      'Each state of mind is a temporary state of mind, happiness or sadness, learn to accept its a never ending cycle - now you\'re unstobbable',
+      'Each state of mind is a temporary state of mind, happiness or sadness, learn to accept its a never ending cycle - only then you\'re unstobbable',
       'Space follows all of its laws, in the ideal society - everyone would follow the law. Your mind has laws, follow them',
       'If you really want something, imagine you have it - then it will run to you, instead of you chasing it',
-      'Remove the possibility of failure, now failure will not occur',
-      'Every adversity will always be a disadvantage. Take control your mind, which will take control of your life, now its your unique advantage',
+      'Remove the possibility of failure, now failure will not occur. Only see success, now only success will occur',
+      'Every adversity will always be a disadvantage. Take control your mind, now its your unique advantage, now you have control over your life',
       '1 of 2 things in the world is energy. 2 forms of energy are positive and negative, choose which mindset you will accquire, there is no inbetween'
    ],
 };
