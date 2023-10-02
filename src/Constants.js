f159751be6fa58cef558ca268f320f83000f68e8
Haseeb <me@haseeb.world>
2023-10-02 13:09:22 +0100

    additional quotes

--- src/Constants.js
+++ src/Constants.js
@@ -8,13 +8,17 @@ module.exports = {
    },
    quotes: [
       'Your dreams are just a mere representation of your future - only if you believe enough',
+      'Why allow yourself to be affected by outer relations. Why allow your inner to flow freely, no direction or purpose in life',
       'You are either a leader or follower, the only difference is the way of thought. Be open minded to the abstract',
       'Each state of mind is a temporary state of mind, happiness or sadness, learn to accept its a never ending cycle - only then you\'re unstobbable',
       'Space follows all of its laws, in the ideal society - everyone would follow the law. Your mind has laws, follow them',
       'If you really want something, imagine you have it - then it will run to you, instead of you chasing it',
       'Remove the possibility of failure, now failure will not occur. Only see success, now only success will occur',
+      'Life is similar to a boomerang, however you perceive is what you will receive',
       'Every adversity will always be a disadvantage. Take control your mind, now its your unique advantage, now you have control over your life',
+      'If you have an opinion on everything, ask yourself what is bothering you?',
       '1 of 2 things in the world is energy. 2 forms of energy are positive and negative, choose which mindset you will accquire, there is no inbetween',
       'If you cannot visualise your goal in detail, then your past is your future, "one day" will never come, that day has to be today, right now',
+      'To hold onto the past, is comparable to multiple blocked roads in your mind',
    ],
 };
