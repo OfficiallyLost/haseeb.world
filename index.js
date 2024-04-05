99fdb02df1eac87c39ac1874d37e79eafd5580d2
Haseeb <me@haseeb.world>
2024-04-05 02:14:42 +0100

    slight changes

--- index.js
+++ index.js
@@ -15,7 +15,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.render('home', { constants }));
-app.get('/quotes', (req, res) => res.render('quotes', { constants }));
+
+app.get('/quotes/:id', (req, res) => {
+    const quote = constants.quotes[req.params.id]
+    const keys = Object.keys(constants.quotes);
+    let key;
+    console.log(keys)
+
+
+
+    console.log('find ' + keys.find((key) => req.params.id))
+
+    if (keys.includes(req.params.id)) {
+        console.log('yes' + req.params.id)
+        key = req.params.id;
+    } else {
+        console.log('no')
+    }
+
+    res.render('quotes', { quote, constants, key })
+});
+
+app.get('/quotes', (req, res) => {
+    res.render('quotes', { constants });
+})
+
 app.get('/youtube/', (req, res) => res.redirect('https://youtube.com/@HaseebMF'));
 
 haseeb.on();
